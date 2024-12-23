028df2ca89ff5bdabb75b3080b1d5f9bf10c5fe9
Kwizera Pacifique <kwizerapacifique19@gmail.com>
2024-12-23 16:57:45 +0200

    init commit

---
+++ src/main/java/com/books/spring_boot_book_store/SpringBootBookStoreApplication.java
@@ -0,0 +1,13 @@
+package com.books.spring_boot_book_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBookStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBookStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/books/spring_boot_book_store/SpringBootBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.books.spring_boot_book_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
